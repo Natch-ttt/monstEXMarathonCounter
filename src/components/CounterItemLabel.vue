ad9f26549ab63252446e17cba41a98b38c501764
Natch <ttt.natsuki@gmail.com>
2025-06-25 18:36:37 +0900

    Upd: home design

---
+++ src/components/CounterItemLabel.vue
@@ -0,0 +1,14 @@
+<template>
+  <ion-label>
+    <h2>{{ name }}</h2>
+    <p>{{ count }} 回</p>
+  </ion-label>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string
+  count: number
+}
+const props = defineProps<Props>()
+</script>
