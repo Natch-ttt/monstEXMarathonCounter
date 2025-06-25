72ebd787450258d6b945f46cff8dcfaec98187bc
Natch <ttt.natsuki@gmail.com>
2025-06-25 18:36:03 +0900

    Add: counter route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,18 @@
 import { createRouter, createWebHistory, createWebHashHistory  } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage    from '@/views/HomePage.vue'
+import CounterPage from '@/views/CounterPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+  { path: '/', redirect: '/home' },
+  { path: '/home',    name: 'Home',    component: HomePage },
+  { path: '/counter/:id', name: 'Counter', component: CounterPage, props: true },
 ]
 
 export const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(),   // ← Hash モードに
-  routes: [
-    { path: '/', redirect: '/home' },
-    { path: '/home', name: 'Home', component: HomePage }
-  ]
+  routes
 })
 
 export default router
